1aea4f8c8a25a23a426d6324738de68a3df6e195
vasil <vasil.sokolov@gmail.com>
2016-12-26 15:08:23 +0300

    fixed annotations

--- classes/group-contact.php
+++ classes/group-contact.php
@@ -15,10 +15,12 @@
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program. If not, see <http://www.gnu.org/licenses/>
  * 
- * @package Modules
  */
 
 /**
+ * @package Classes
+ * @subpackage GroupContact
+ * 
  * @property string $GroupUUID
  * @property string $ContactUUID
  */
