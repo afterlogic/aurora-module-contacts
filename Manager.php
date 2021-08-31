4cf25f4bfd56e8c8ece520cf06503c143c2ab7f6
sash04ek <a.ovcharov@afterlogic.com>
2021-08-31 08:45:07 +0300

    fixed issue with sorting order

--- Manager.php
+++ Manager.php
@@ -349,7 +349,7 @@ class Manager extends \Aurora\System\Managers\AbstractManager
 			->where($aFilters)
 			->offset($iOffset)
 			->limit($iLimit)
-			->orderBy([$sSortField])
+			->orderBy([$sSortField, 'ViewEmail'])
 			->sortOrder($iSortOrder)
 			->asArray()
 			->exec();
