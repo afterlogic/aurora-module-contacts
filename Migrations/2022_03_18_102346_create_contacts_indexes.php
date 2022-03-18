17d7b02d6c6305bfc211f768e37a225862e0a2d9
sash04ek <a.ovcharov@afterlogic.com>
2022-03-18 15:53:03 +0300

    added indexes

---
+++ Migrations/2022_03_18_102346_create_contacts_indexes.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class CreateContactsIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Capsule::schema()->table('contacts', function(Blueprint $table)
+        {
+            $table->index('IdUser');
+        });
+
+        Capsule::schema()->table('contacts_groups', function(Blueprint $table)
+        {
+            $table->index('IdUser');
+        });
+
+        Capsule::schema()->table('contacts_addressbooks', function(Blueprint $table)
+        {
+            $table->index('UserId');
+        });
+
+        Capsule::schema()->table('contacts_ctags', function(Blueprint $table)
+        {
+            $table->index('UserId');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Capsule::schema()->table('contacts', function (Blueprint $table)
+        {
+            $table->dropIndex(['IdUser']);
+        });
+
+        Capsule::schema()->table('contacts_groups', function (Blueprint $table)
+        {
+            $table->dropIndex(['IdUser']);
+        });
+
+        Capsule::schema()->table('contacts_addressbooks', function (Blueprint $table)
+        {
+            $table->dropIndex(['UserId']);
+        });
+
+        Capsule::schema()->table('contacts_ctags', function (Blueprint $table)
+        {
+            $table->dropIndex(['UserId']);
+        });
+    }
+}
