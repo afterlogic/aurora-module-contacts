c14157001c431a55537ff199e1d40f710e25cdb5
sash04ek <a.ovcharov@afterlogic.com>
2022-06-23 08:20:00 +0300

    fixed - BUG - contact is imported to Personal storage while its importing to address book

--- Classes/Csv/Sync.php
+++ Classes/Csv/Sync.php
@@ -131,13 +131,9 @@ class Sync
 
 					$iParsedCount++;
 
-					// if (isset($sStorage)) {
-					// 	$aStorageParts = \explode('-', $sStorage);
-					// 	if (count($aStorageParts) === 2 && $aStorageParts[0] === StorageType::AddressBook) {
-					// 		$aContactData['Storage'] = StorageType::AddressBook;
-					// 		$aContactData['AddressBookId'] = $aStorageParts[1];
-					// 	}
-					// }
+					if (isset($sStorage)) {
+					 	$aContactData['Storage'] = $sStorage;
+					}
 					
 					$oContactsDecorator = \Aurora\Modules\Contacts\Module::Decorator();
 					if ($oContactsDecorator && $oContactsDecorator->CreateContact($aContactData, $iUserId))
