27dac42bf614448a0ef0fd585f4fa57d3990f6ec
Vasil Sokolov <vasil@afterlogic.com>
2023-11-22 11:37:00 +0200

    Added indexes to the contacts_cards table

--- Migrations/2023_09_26_144011_create_contacts_cards_table.php
+++ Migrations/2023_09_26_144011_create_contacts_cards_table.php
@@ -31,6 +31,34 @@ class CreateContactsCardsTable extends Migration
 
             $table->json('Properties')->nullable();
         });
+
+        $prefix = Capsule::connection()->getTablePrefix();
+
+        Capsule::schema()->table('contacts_cards', function (Blueprint $table) use ($prefix) {
+            $table->index('CardId');
+            $table->index('AddressBookId');
+            $table->index('IsGroup');
+            $table->index('Frequency');
+
+            Capsule::connection()->statement(
+                "CREATE FULLTEXT INDEX contacts_cards_fullname_index ON {$prefix}contacts_cards (FullName)"
+            );
+            Capsule::connection()->statement(
+                "CREATE FULLTEXT INDEX contacts_cards_viewemail_index ON {$prefix}contacts_cards (ViewEmail)"
+            );
+            Capsule::connection()->statement(
+                "CREATE FULLTEXT INDEX contacts_cards_personalemail_index ON {$prefix}contacts_cards (PersonalEmail)"
+            );
+            Capsule::connection()->statement(
+                "CREATE FULLTEXT INDEX contacts_cards_businessemail_index ON {$prefix}contacts_cards (BusinessEmail)"
+            );
+            Capsule::connection()->statement(
+                "CREATE FULLTEXT INDEX contacts_cards_businesscompany_index ON {$prefix}contacts_cards (BusinessCompany)"
+            );
+            Capsule::connection()->statement(
+                "CREATE FULLTEXT INDEX contacts_cards_otheremail_index ON {$prefix}contacts_cards (OtherEmail)"
+            );
+        });
     }
 
     /**
