827700cab3ee6847a8285d40e2a0154647b3ce33
sash04ek <a.ovcharov@gmail.com>
2023-12-28 16:46:33 +0300

    improved migration script

--- Migrations/2023_12_26_124548_copy_data_to_contacts_cards_table.php
+++ Migrations/2023_12_26_124548_copy_data_to_contacts_cards_table.php
@@ -10,6 +10,7 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 
 class CopyDataToContactsCardsTable extends Migration
 {
+    protected $filterByAddressBooks = false;
     /**
      * Run the migrations.
      *
@@ -21,43 +22,74 @@ class CopyDataToContactsCardsTable extends Migration
         Capsule::connection()->table('contacts')->orderBy('Id')->chunk(100000, function ($rows) {
             foreach ($rows as $row) {
                 try {
-                    $properties = json_decode($row->Properties, true);
-                    $uid = $row->UUID;
-                    if (isset($properties['DavContacts::UID'])) {
-                        $uid = $properties['DavContacts::UID'];
-                        unset($properties['DavContacts::UID']);
-                    }
-                    if (isset($properties['DavContacts::VCardUID'])) {
-                        unset($properties['DavContacts::VCardUID']);
-                    }
-
-                    $userPrincipal = Constants::PRINCIPALS_PREFIX . Api::getUserPublicIdById($row->IdUser);
-                    $storage = $row->Storage;
-                    $storagesMapToAddressbooks = \Aurora\Modules\Contacts\Module::Decorator()->GetStoragesMapToAddressbooks();
-                    $addressbookUri = null;
-                    $isCustomAddressBook = false;
-                    if (isset($storagesMapToAddressbooks[$storage])) {
-                        $addressbookUri = $storagesMapToAddressbooks[$storage];
-                    } elseif ($row->AddressBookId) {
-                        $isCustomAddressBook = true;
+                    $properties = null;
+                    if (isset($row->Properties)) {
+                        $properties = json_decode($row->Properties, true);
+                        $uid = $row->UUID;
+                        if (isset($properties['DavContacts::UID'])) {
+                            $uid = $properties['DavContacts::UID'];
+                            unset($properties['DavContacts::UID']);
+                        }
+                        if (isset($properties['DavContacts::VCardUID'])) {
+                            unset($properties['DavContacts::VCardUID']);
+                        }
                     }
 
-                    $query = Capsule::connection()->table('contacts_cards')->select('contacts_cards.Id')
-                        ->join('adav_addressbooks', 'contacts_cards.AddressBookId', '=', 'adav_addressbooks.id')
+                    $userPublicId = Api::getUserPublicIdById($row->IdUser);
+                    $userPrincipal = Constants::PRINCIPALS_PREFIX . $userPublicId;
+
+                    if (isset($properties) || $row->Frequency > 0) {
+                        $query = Capsule::connection()->table('contacts_cards')
+                            ->select('contacts_cards.Id', 'contacts_cards.CardId', 'contacts_cards.Properties')
+                            ->join('adav_addressbooks', 'contacts_cards.AddressBookId', '=', 'adav_addressbooks.id')
                             ->join('adav_cards', 'contacts_cards.CardId', '=', 'adav_cards.id')
-                                ->where('adav_addressbooks.principaluri', $userPrincipal)
-                                ->where('adav_cards.uri', $uid . '.vcf');
+                            ->where('adav_addressbooks.principaluri', $userPrincipal)
+                            ->where('contacts_cards.ViewEmail', $row->ViewEmail)
+                            ->where('adav_cards.uri', $uid . '.vcf');
 
-                    if ($isCustomAddressBook) {
-                        $query->join('contacts_addressbooks', 'contacts_addressbooks.UUID', '=', 'adav_addressbooks.uri')
-                            ->where('contacts_addressbooks.Id', $row->AddressBookId);
-                    } elseif ($addressbookUri) {
-                        $query->where('adav_addressbooks.uri', $addressbookUri);
-                    }
-                    $ids = $query->pluck('Id')->toArray();
+                        if ($this->filterByAddressBooks) {
+                            $storage = $row->Storage;
+                            $storagesMapToAddressbooks = \Aurora\Modules\Contacts\Module::Decorator()->GetStoragesMapToAddressbooks();
+                            $addressbookUri = null;
+                            $isCustomAddressBook = false;
+                            if (isset($storagesMapToAddressbooks[$storage])) {
+                                $addressbookUri = $storagesMapToAddressbooks[$storage];
+                            } elseif ($row->AddressBookId) {
+                                $isCustomAddressBook = true;
+                            }
 
-                    if (count($ids) > 0) {
-                        ContactCard::whereIn('Id', $ids)->update(['Properties' => \json_encode($properties), 'Frequency' => $row->Frequency]);
+                            if ($isCustomAddressBook) {
+                                $query->join('contacts_addressbooks', 'contacts_addressbooks.UUID', '=', 'adav_addressbooks.uri')
+                                    ->where('contacts_addressbooks.Id', $row->AddressBookId);
+                            } elseif ($addressbookUri) {
+                                $query->where('adav_addressbooks.uri', $addressbookUri);
+                            }
+                        }
+                        $cardsInfo = $query->get();
+
+                        foreach ($cardsInfo as $info) {
+                            $update = [];
+                            if (isset($properties)) {
+                                if (isset($info->Properties)) {
+                                    $oldProperties = \json_decode($info->Properties, true);
+                                    $properties = array_merge($oldProperties, $properties);
+                                }
+                                $update['Properties'] = \json_encode($properties);
+                            }
+                            if ($row->Frequency > 0) {
+                                $update['Frequency'] = $row->Frequency;
+                            }
+                            if (count($update) > 0) {
+                                if (!!ContactCard::where('Id', $info->Id)->update($update)) {
+                                    Api::Log('Contact migrated successfuly:', \Aurora\System\Enums\LogLevel::Full, 'contacts-migration-');
+                                    Api::Log('User.PublicId: ' . $userPublicId, \Aurora\System\Enums\LogLevel::Full, 'contacts-migration-');
+                                    Api::Log('Contact.UUID: ' . $uid, \Aurora\System\Enums\LogLevel::Full, 'contacts-migration-');
+                                    Api::Log('Conatct.ViewEmail: ' . $row->ViewEmail, \Aurora\System\Enums\LogLevel::Full, 'contacts-migration-');
+                                    Api::Log('ContactCard.CardId: ' . $info->CardId, \Aurora\System\Enums\LogLevel::Full, 'contacts-migration-');
+                                    Api::Log('', \Aurora\System\Enums\LogLevel::Full, 'contacts-migration-');
+                                }
+                            }
+                        }
                     }
                 } catch (\Exception $e) {
                     Api::Log('Contact migration exception', \Aurora\System\Enums\LogLevel::Error, 'contacts-migration-');
